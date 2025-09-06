7b312d7df4eca9d10738d4d622eb8b174b111bf3
Marat Kuzakhmetov <kuzakhmetovmarat@gmail.com>
2025-09-06 15:47:53 +0200

    fixed mainPage mainText block to blockContent

--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -1,9 +1,11 @@
 import globalSettings from './globalSettings'
 import mainPage from './mainPage'
 import {contentBlockType} from './contentBlockType'
+import blockContent from '../schemas/objects/blockContent'
 
 export const schemaTypes = [
   globalSettings,
+  blockContent,
   mainPage,
   contentBlockType('En'),
   contentBlockType('Ru'),
---
+++ schemas/objects/blockContent.ts
@@ -0,0 +1,26 @@
+import {defineType} from 'sanity'
+
+export default defineType({
+  name: 'blockContent',
+  title: 'Block Content',
+  type: 'array',
+  of: [
+    {
+      type: 'block',
+      marks: {
+        decorators: [
+          {title: 'Strong', value: 'strong'},
+          {title: 'Emphasis', value: 'em'},
+        ],
+        annotations: [
+          {
+            name: 'link',
+            type: 'object',
+            title: 'Link',
+            fields: [{name: 'href', type: 'url', title: 'URL'}],
+          },
+        ],
+      },
+    },
+  ],
+})
